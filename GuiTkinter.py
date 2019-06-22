ca8c54bf246e90b5aad337541924aa34b3518c49
達也 鴛海 <tatsuya.oshinomi.0828@gmail.com>
2019-06-22 18:59:05 +0900

    プロジェクト ファイルを追加します。

---
+++ GuiTkinter.py
@@ -0,0 +1,15 @@
+import tkinter
+from tkinter import messagebox
+
+root = tkinter.Tk()
+root.title(u"Hello World")
+root.geometry("400x300")
+
+def OnMessageBox(event):
+  messagebox.showinfo('確認', 'Hello World')
+
+Button = tkinter.Button(text=u'メッセージボックスを表示します', width=50)
+Button.bind("<Button>", OnMessageBox) 
+Button.pack()
+
+root.mainloop()
